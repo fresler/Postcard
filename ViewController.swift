02eea70b43e0309a8242f122b5ab17e621de123f
Floyd Resler <macmage2@mac.com>
2014-10-02 08:55:31 -0400

    Initial import
    Initial import

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Floyd Resler on 9/30/14.
+//  Copyright (c) 2014 Floyd Resler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //Comments
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden=false
+        messageLabel.text=enterMessageTextField.text
+        messageLabel.textColor=UIColor.redColor()
+        enterMessageTextField.text=""
+        enterMessageTextField.resignFirstResponder()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+
+}
+
