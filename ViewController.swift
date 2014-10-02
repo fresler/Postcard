372700c3ff696c9d1386f296a16c930a77f1f3db
Floyd Resler <macmage2@mac.com>
2014-10-02 09:15:24 -0400

    Challenge 1
    After first challenge

--- ViewController.swift
+++ ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    //Comments
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,6 +32,11 @@ class ViewController: UIViewController {
         messageLabel.textColor=UIColor.redColor()
         enterMessageTextField.text=""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.hidden=false
+        nameLabel.text=enterNameTextField.text
+        nameLabel.textColor=UIColor.blackColor()
+        enterNameTextField.text=""
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
 
